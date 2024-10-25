12fb520620c45a759b24b7b6bf38ab29e7a5cb28
romanvht <romanvht@gmail.com>
2024-10-25 18:02:00 +0400

    Edit main settings fragment

--- app/src/main/java/io/github/dovecoteescapee/byedpi/fragments/MainSettingsFragment.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/fragments/MainSettingsFragment.kt
@@ -107,9 +107,7 @@ class MainSettingsFragment : PreferenceFragmentCompat() {
     override fun onResume() {
         super.onResume()
         sharedPreferences?.registerOnSharedPreferenceChangeListener(preferenceListener)
-
-        val accessibilityStatusPref = findPreference<Preference>("accessibility_service_status")
-        updateAccessibilityStatus(accessibilityStatusPref)
+        updatePreferences()
     }
 
     override fun onPause() {
@@ -125,6 +123,9 @@ class MainSettingsFragment : PreferenceFragmentCompat() {
 
         val applist_type = findPreferenceNotNull<ListPreference>("applist_type")
         val selected_apps = findPreferenceNotNull<Preference>("selected_apps")
+        val accessibilityStatusPref = findPreferenceNotNull<Preference>("accessibility_service_status")
+
+        updateAccessibilityStatus(accessibilityStatusPref)
 
         when (mode) {
             Mode.VPN -> {
