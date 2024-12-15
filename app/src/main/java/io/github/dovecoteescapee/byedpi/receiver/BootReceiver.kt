809a6b257137f6841af5851eae773f8bf3ae30a7
romanvht <romanvht@gmail.com>
2024-12-16 00:22:03 +0400

    edit BootReceiver

--- app/src/main/java/io/github/dovecoteescapee/byedpi/receiver/BootReceiver.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/receiver/BootReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.VpnService
+import android.os.SystemClock
 import io.github.dovecoteescapee.byedpi.data.Mode
 import io.github.dovecoteescapee.byedpi.services.ServiceManager
 import io.github.dovecoteescapee.byedpi.utility.getPreferences
@@ -14,6 +15,11 @@ class BootReceiver : BroadcastReceiver() {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
             intent.action == Intent.ACTION_REBOOT ||
             intent.action == "android.intent.action.QUICKBOOT_POWERON") {
+
+            // for A15, todo: use wasForceStopped
+            if (SystemClock.elapsedRealtime() > 5 * 60 * 1000) {
+                return
+            }
 
             val preferences = context.getPreferences()
             val autorunEnabled = preferences.getBoolean("autostart", false)
