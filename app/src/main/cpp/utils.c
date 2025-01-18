b3a6b0ab21d263340b1215a92729e5c705399de0
romanvht <romanvht@gmail.com>
2025-01-18 13:32:37 +0400

    update byedpi to 7b46e59

--- app/src/main/cpp/utils.c
+++ app/src/main/cpp/utils.c
@@ -194,10 +194,10 @@ int parse_args(int argc, char **argv)
                 while (end && !invalid) {
                     switch (*end) {
                         case 't':
-                            dp->proto |= IS_HTTPS;
+                            dp->proto |= IS_TCP | IS_HTTPS;
                             break;
                         case 'h':
-                            dp->proto |= IS_HTTP;
+                            dp->proto |= IS_TCP | IS_HTTP;
                             break;
                         case 'u':
                             dp->proto |= IS_UDP;
